ec8b1a8edf9cf72f19171f5caeccf91c05874377
Bokyung Kwon <tree445560@gmail.com>
2024-08-30 18:07:31 +0900

    add: 글자수 제한 기능과 마지막으로 작성한 글자 삭제 기능 추가

--- src/main.js
+++ src/main.js
@@ -2,8 +2,22 @@ let buttons = document.querySelectorAll('button');
 
 buttons.forEach((button) => {
   button.addEventListener('click', () => {
-    if (button.value === 'clear') {
+    if (button.value === 'equals') {
+      input.value = eval(input.value);
+    } else if (button.value === 'clear') {
       input.value = '';
+    } else if (button.value === 'divide') {
+      input.value += '/';
+    } else if (button.value === 'multi') {
+      input.value += '*';
+    } else if (button.value === 'minus') {
+      input.value += '-';
+    } else if (button.value === 'plus') {
+      input.value += '+';
+    } else if (button.value === 'delete') {
+      input.value = input.value.slice(0, -1);
+    } else if (input.value.length == 13) {
+      alert('더 이상 입력할 수 없습니다.');
     } else {
       input.value += button.value;
     }
